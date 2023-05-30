0b01b050f99d32972ca7234dee53bd7dd38c4a55
Ellie Spigelman <ellie@alignable.com>
2023-05-30 10:26:44 -0400

    merged master

--- bifrost/proxy/pages/+config.ts
+++ bifrost/proxy/pages/+config.ts
@@ -2,10 +2,10 @@ import { type ConfigNonHeaderFile } from "vite-plugin-ssr/types";
 
 export default {
   route: "/*",
-  onBeforeRenderPath: "@alignable/bifrost/proxy/pages/onBeforeRender",
-  onRenderClientPath: "@alignable/bifrost/proxy/pages/onRenderClient",
-  onRenderHtmlPath: "@alignable/bifrost/proxy/pages/onRenderHtml",
-  PagePath: "@alignable/bifrost/proxy/pages/Page",
+  onBeforeRender: "import:@alignable/bifrost/proxy/pages/onBeforeRender",
+  onRenderClient: "import:@alignable/bifrost/proxy/pages/onRenderClient",
+  onRenderHtml: "import:@alignable/bifrost/proxy/pages/onRenderHtml",
+  Page: "import:@alignable/bifrost/proxy/pages/Page",
   passToClient: ["proxySendClient", "layout", "layoutProps", "redirectTo"],
   clientRouting: true,
   hydrationCanBeAborted: true,
