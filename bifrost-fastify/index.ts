1f2a1c8a0beb6a6ca9442b1401bc4f1311d6976f
Jacob To <90359975+jacobto161@users.noreply.github.com>
2023-08-04 14:38:14 -0400

    rebase

--- bifrost-fastify/index.ts
+++ bifrost-fastify/index.ts
@@ -103,12 +103,6 @@ export const viteProxyPlugin: FastifyPluginAsync<
           { _pageId: string },
           typeof pageContextInit
         >(pageContextInit);
-        
-        if (req.url.endsWith('/')) {
-          req.log.info("bifrost: redirecting trailing slash");
-          reply.redirect(301, req.url.slice(0, -1));
-          return;
-        }
 
         const proxy = pageContext._pageId === proxyPageId;
         const noRouteMatched =
