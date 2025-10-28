a56ec1e102389f9f95305fd575f78028a84c58a9
Da-Jin Chu <dajinchu@gmail.com>
2025-10-28 17:09:19 -0400

    Procfile for Dev + Playwright CI (#30)

--- bifrost/tsup.config.ts
+++ bifrost/tsup.config.ts
@@ -1,4 +1,18 @@
 import { defineConfig } from "tsup";
+import { resolve, parse, format } from "path";
+import { readdir, rename } from "fs/promises";
+
+async function* getFiles(dir: string): AsyncGenerator<string> {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
 
 export default defineConfig({
   entry: ["./index.ts", "./renderer/**/*.ts?(x)", "./proxy/**/*.ts?(x)"],
@@ -6,4 +20,16 @@ export default defineConfig({
   clean: true,
   sourcemap: true,
   dts: true,
+  async onSuccess() {
+    for await (const f of getFiles("./dist")) {
+      const parsed = parse(f);
+      if (parsed.base.startsWith("_") && parsed.ext === ".ts") {
+        const newname = format({
+          ...parsed,
+          base: parsed.base.replace("_", "+"),
+        });
+        await rename(f, newname);
+      }
+    }
+  },
 });
--- tests/alb/index.mjs
+++ tests/alb/index.mjs
@@ -1,7 +1,7 @@
-const proxy = require("redbird")({ port: 5050 });
+import { Redbird } from "redbird";
 
 const LEGACY_URL = "http://localhost:5557";
-const BIFROST_URL = "http://localhost:5555";
+const BIFROST_URL = `http://localhost:${process.env.BIFROST_PORT || 5555}`;
 const BIFROST_PATHS = [
   "/bifrost-assets",
   "/vite-page",
@@ -18,6 +18,12 @@ const BIFROST_PATHS = [
   "/",
 ];
 
+console.log(
+  `Proxying Bifrost requests to ${BIFROST_URL} and legacy to ${LEGACY_URL}`
+);
+console.log(`ALB up on http://localhost:5050`);
+
+const proxy = new Redbird({ port: 5050, cluster: 1, keepAlive: true });
 proxy.addResolver((host, url, req) => {
   if (req.headers["x-vite-proxy"]) return LEGACY_URL;
 
