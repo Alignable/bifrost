c9aa38640abb33c09ed8f9f8f387c409241ebf77
Da-Jin Chu <dajinchu@gmail.com>
2025-10-30 13:26:50 -0400

    temp: flexible currentNav

--- tests/vite/layouts/MainNavLayout.tsx
+++ tests/vite/layouts/MainNavLayout.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
 
-export const MainNavLayout = ({ children }: { children: React.ReactNode }) => {
-  const {
-    config: { currentNav },
-  } = usePageContext();
+export const MainNavLayout = ({
+  children,
+  currentNav,
+}: {
+  children: React.ReactNode;
+  currentNav?: string;
+}) => {
+  const pageContext = usePageContext();
   return (
     <div>
       <nav>Main Nav Layout vite</nav>
-      <p>selected: {currentNav}</p>
+      <p>selected: {pageContext?.config?.currentNav || currentNav}</p>
       {children}
     </div>
   );
--- tests/vite/layouts/VisitorLayout.tsx
+++ tests/vite/layouts/VisitorLayout.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
 
-export const VisitorLayout = ({ children }: { children: React.ReactNode }) => {
-  const {
-    config: { currentNav },
-  } = usePageContext();
+export const VisitorLayout = ({
+  children,
+  currentNav,
+}: {
+  children: React.ReactNode;
+  /// TODO: move to page context and adopt vike-react in bifrost?
+  currentNav?: string;
+}) => {
+  const pageContext = usePageContext();
   return (
     <div>
       <nav>visitor layout</nav>
-      <p>selected: {currentNav}</p>
+      <p>selected: {pageContext?.config?.currentNav || currentNav}</p>
       {children}
     </div>
   );
