9a087c0e7df96fae1ab6ceedb2e0b3e5b57b131a
Da-Jin Chu <dajinchu@gmail.com>
2025-11-21 09:41:57 -0500

    suspend in tests

--- tests/vite/pages/stream-page/+Page.tsx
+++ tests/vite/pages/stream-page/+Page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { use } from "react";
 
 export default function Page() {
+  use(new Promise((resolve) => setTimeout(resolve, 10)));
   return (
     <>
       <h1>streamed page</h1>
