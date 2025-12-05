e15a6579e111b884097a6296872a0682f0ead156
Da-Jin Chu <dajinchu@gmail.com>
2025-12-05 11:39:55 -0500

    fix?

--- tests/e2e/specs/e2e.spec.ts
+++ tests/e2e/specs/e2e.spec.ts
@@ -883,7 +883,7 @@ test.describe("turbolinks: events", () => {
 
 test("prefetch()", async ({ page }) => {
   const headTestAssetPromise = page.waitForRequest(
-    "**/bifrost-assets/pages/head-test/+Page.tsx"
+    "**/pages/head-test/+Page.tsx"
   );
   await page.goto("./vite-page");
   // Preload loads this automatically
