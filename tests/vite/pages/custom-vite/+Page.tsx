be36dc2a0cd3b23a9ed5c5488692d55bdf99b1ad
Da-Jin Chu <dajinchu@gmail.com>
2026-01-13 14:46:37 -0500

    pagedata specfix

--- tests/vite/pages/custom-vite/+Page.tsx
+++ tests/vite/pages/custom-vite/+Page.tsx
@@ -4,22 +4,24 @@ import { usePageContext } from "vike-react/usePageContext";
 
 export default function Page() {
   const { urlParsed } = usePageContext();
+  const pageData = urlParsed.search["page"];
   return (
     <>
       <h1>vite is here</h1>
-      {(JSON.parse(urlParsed.search["page"]) as PageDataOk)?.links?.map(
-        (link) =>
-          "title" in link && (
-            <a
-              href={`/${link.endpoint || "custom"}?page=${encodeURI(
-                JSON.stringify(link)
-              )}`}
-              key={link.title}
-            >
-              {link.title}
-            </a>
-          )
-      )}
+      {pageData &&
+        (JSON.parse(pageData) as PageDataOk)?.links?.map(
+          (link) =>
+            "title" in link && (
+              <a
+                href={`/${link.endpoint || "custom"}?page=${encodeURI(
+                  JSON.stringify(link)
+                )}`}
+                key={link.title}
+              >
+                {link.title}
+              </a>
+            )
+        )}
     </>
   );
 }
