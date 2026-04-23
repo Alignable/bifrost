a13eba8d23ac43d803935460b70a0cd18666da77
Reina To <90359975+reinabo@users.noreply.github.com>
2026-04-23 11:32:06 -0400

    (#92) Fix error message leaking into error page

--- bifrost/renderer/wrapped/onAfterRenderHtml.ts
+++ bifrost/renderer/wrapped/onAfterRenderHtml.ts
@@ -1,5 +1,4 @@
 import { PageContextServer } from "vike/types";
-import { render } from "vike/abort";
 
 export default function wrappedOnAfterRenderHtml(
   pageContext: PageContextServer
@@ -9,8 +8,7 @@ export default function wrappedOnAfterRenderHtml(
     !pageContext._wrappedServerOnly.renderedBody
   ) {
     // We set wrapped serverOnly but we never rendered the body
-    throw render(
-      500,
+    throw new Error(
       "proxied-body not found in DOM after SSR. This likely means the Layout threw during SSR (e.g. accessing `window` or `document`). Fix the SSR error in your Layout component."
     );
   }
